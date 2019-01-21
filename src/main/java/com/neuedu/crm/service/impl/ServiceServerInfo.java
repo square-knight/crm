2b11d5fea19896a9e0c1e53245cbe0524d56a4af
fuxinpeng <xinpeng.fu@shoufuyou.com>
2019-01-21 15:44:25 +0800

    delete device info

--- src/main/java/com/neuedu/crm/service/impl/ServiceServerInfo.java
+++ src/main/java/com/neuedu/crm/service/impl/ServiceServerInfo.java
@@ -43,9 +43,7 @@ public class ServiceServerInfo implements IServerInfoService{
             serverInfo = new ServerInfo();
         }
 
-        try { 
-            
-            SigarUtils.initSigar();
+        try {
             //服务信息
             Properties props=System.getProperties();
             Runtime runTime = Runtime.getRuntime();
@@ -64,49 +62,7 @@ public class ServiceServerInfo implements IServerInfoService{
             Calendar cal = Calendar.getInstance();
             TimeZone timeZone = cal.getTimeZone();
             serverInfo.setOsTimeZone(timeZone.getDisplayName());
-                
 
-            //Cpu
-            Sigar sigar = new Sigar();
-            CpuInfo cpuInfos[] = sigar.getCpuInfoList();
-            serverInfo.setCpuNum(cpuInfos.length);
-            CpuPerc cpuList[] = sigar.getCpuPercList();
-            List<CpuInfoVo> cpuVoList = new ArrayList<CpuInfoVo>();
-            for(int m=0;m<cpuList.length;m++){
-                CpuInfoVo cpuInfoVo = new CpuInfoVo();
-                cpuInfoVo.setCpuMhz(cpuInfos[m].getMhz());
-                String cpuIdle = String.format("%.2f",cpuList[m].getIdle()*100)+"%";
-                cpuInfoVo.setCpuIdle(cpuIdle);
-                String cpuCombined = String.format("%.2f",cpuList[m].getCombined()*100)+"%";
-                cpuInfoVo.setCpuCombined(cpuCombined);
-                cpuVoList.add(cpuInfoVo);
-            }
-            serverInfo.setCpuList(cpuVoList);
-
-                
-            //网卡信息
-            String netInfos[] = sigar.getNetInterfaceList();
-            List<NetInterfaceConfig> netList = new ArrayList<NetInterfaceConfig>();
-            for(int i=0;i<netInfos.length;i++){
-                NetInterfaceConfig netInfo = sigar.getNetInterfaceConfig(netInfos[i]); 
-                if((NetFlags.LOOPBACK_ADDRESS.equals(netInfo.getAddress()))     //127.0.0.1 
-                        || (netInfo.getFlags() == 0)                            //标识为0
-                        || (NetFlags.NULL_HWADDR.equals(netInfo.getHwaddr()))   //MAC地址不存在
-                        || (NetFlags.ANY_ADDR.equals(netInfo.getAddress()))     //0.0.0.0
-                        ){
-                    continue;
-                }
-                netList.add(netInfo);
-            }
-            serverInfo.setNetNum(netList.size());
-            serverInfo.setNetList(netList);
-                
-            //物理内存
-            Mem mem = sigar.getMem();  
-            serverInfo.setMemTotal((mem.getTotal()/(1024*1024))+"M");
-            serverInfo.setMemUsed((mem.getUsed()/(1024*1024))+"M");
-            serverInfo.setMemFree((mem.getFree()/(1024*1024))+"M");
-                
             //java配置
             serverInfo.setJavaPath(props.getProperty("java.home"));
             serverInfo.setJavaVendor(props.getProperty("java.vendor"));
@@ -115,15 +71,12 @@ public class ServiceServerInfo implements IServerInfoService{
             serverInfo.setJvmVersion(props.getProperty("java.vm.version"));
             serverInfo.setJvmTotalMemory((int)(runTime.totalMemory()/(1024*1024))+"M");
             serverInfo.setJvmFreeMemory((int)(runTime.freeMemory()/(1024*1024))+"M");
-            
+
             //数据库信息
             findDatabase(serverInfo);
-            
 
         } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (SigarException e) {
-            e.printStackTrace();
+            System.out.println(e);
         }
         return serverInfo;
         
@@ -146,7 +99,7 @@ public class ServiceServerInfo implements IServerInfoService{
             serverInfo.setDatabaseDriver(metaData.getDriverName());
             serverInfo.setDriverVersion(metaData.getDriverVersion());
         }catch(Exception e){
-            e.printStackTrace();
+            System.out.println(e);
         }
     }
 
@@ -170,7 +123,7 @@ public class ServiceServerInfo implements IServerInfoService{
                 cpuVoList.add(cpuInfoVo);
             }
         } catch (SigarException e) {
-            e.printStackTrace();
+            System.out.println(e);
         }
         return cpuVoList;
     }
@@ -187,7 +140,7 @@ public class ServiceServerInfo implements IServerInfoService{
             map.put("used", (mem.getUsed()/(1024*1024))+"M");
             map.put("free", (mem.getFree()/(1024*1024))+"M");
         } catch (SigarException e) {
-            e.printStackTrace();
+            System.out.println(e);
         }  
         return map;
     }
